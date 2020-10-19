eea87f503582b159f0984a655e133e27082f0c04
Thomas Fabritius <tfabritius@gmail.com>
2020-10-19 14:55:50 +0200

    Support OpenAPI specification

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  /* Add OpenAPI */
+  const options = new DocumentBuilder()
+    .setTitle('Portfolio Report API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('doc', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
