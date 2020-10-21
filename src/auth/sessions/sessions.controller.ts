f6e3280ee0f30b4e3c6c4f098582833733b0297a
Thomas Fabritius <tfabritius@gmail.com>
2020-10-21 10:33:40 +0200

    List active sessions of current user (/auth/sessions endpoint)

--- src/auth/sessions/sessions.controller.ts
+++ src/auth/sessions/sessions.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, UseGuards } from '@nestjs/common'
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger'
 
-@Controller('sessions')
-export class SessionsController {}
+import { AuthUser } from '../auth.decorator'
+import { DefaultAuthGuard } from '../default-auth.guard'
+import { User } from '../users/user.entity'
+import { Session } from './session.entity'
+import { SessionsService } from './sessions.service'
+
+@Controller('auth/sessions')
+@UseGuards(DefaultAuthGuard)
+@ApiTags('auth/sessions')
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@ApiBadRequestResponse({ description: 'Bad request' })
+export class SessionsController {
+  constructor(private sessionsService: SessionsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get all sessions' })
+  @ApiOkResponse({
+    description: 'List of all sessions is returned.',
+    type: Session,
+    isArray: true,
+  })
+  async readAll(@AuthUser() user: User): Promise<Session[]> {
+    return await this.sessionsService.getAllOfUser(user)
+  }
+}
