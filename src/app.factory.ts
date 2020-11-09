9be586b407587d442ffeceed76552a504c818a73
Thomas Fabritius <tfabritius@gmail.com>
2020-11-09 12:36:37 +0100

    Use factory to create app

---
+++ src/app.factory.ts
@@ -0,0 +1,47 @@
+import { NestFactory, Reflector } from '@nestjs/core'
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { AppModule } from './app.module'
+
+export async function createApp(
+  environment: 'prod' | 'test' = 'prod',
+): Promise<INestApplication> {
+  let app: INestApplication
+
+  if (environment === 'prod') {
+    app = await NestFactory.create(AppModule)
+  } else {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+  }
+
+  /* Validate input and remove unknown properties */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  )
+
+  /* Use class-transformer on output objects */
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+
+  /* Add OpenAPI */
+  const options = new DocumentBuilder()
+    .setTitle('Portfolio Report API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('doc', app, document)
+
+  return app
+}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,7 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
-import { NestFactory, Reflector } from '@nestjs/core'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-import { AppModule } from './app.module'
+import { createApp } from './app.factory'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-
-  /* Validate input and remove unknown properties */
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  )
-
-  /* Use class-transformer on output objects */
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
-
-  /* Add OpenAPI */
-  const options = new DocumentBuilder()
-    .setTitle('Portfolio Report API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('doc', app, document)
+  const app = await createApp()
 
   await app.listen(3000)
 }
