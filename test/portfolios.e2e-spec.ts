48ce166dfcea5790d2f887fd8fb6536f787e486b
Thomas Fabritius <tfabritius@gmail.com>
2020-11-12 21:23:23 +0100

    Restructure portfolios tests

--- test/portfolios.e2e-spec.ts
+++ test/portfolios.e2e-spec.ts
@@ -2,7 +2,11 @@ import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 
 import { createApp } from '../src/app.factory'
-import { getObjectsWithMissingAttribute, registerUser } from './utils'
+import {
+  createTestPortfolio,
+  getObjectsWithMissingAttribute,
+  registerUser,
+} from './utils'
 
 describe('Portfolios (e2e)', () => {
   let app: INestApplication
@@ -33,29 +37,29 @@ describe('Portfolios (e2e)', () => {
     await app.close()
   })
 
-  describe('Without portfolio', () => {
-    test('GET /portfolios returns empty list', async () => {
+  test('GET /portfolios returns empty list', async () => {
+    const response = await request(http)
+      .get('/portfolios')
+      .set('Authorization', 'bearer ' + sessionToken)
+
+    expect(response.status).toBe(200)
+    expect(response.body).toStrictEqual([])
+  })
+
+  test.each([['GET'], ['PUT'], ['DELETE']])(
+    '%s /portfolios/42 returns 404',
+    async (method) => {
       const response = await request(http)
-        .get('/portfolios')
+        [method.toLowerCase()]('/portfolios/42')
         .set('Authorization', 'bearer ' + sessionToken)
 
-      expect(response.status).toBe(200)
-      expect(response.body).toStrictEqual([])
-    })
+      expect(response.status).toBe(404)
+    },
+  )
 
-    test.each([['GET'], ['PUT'], ['DELETE']])(
-      '%s /portfolios/42 returns 404',
-      async (method) => {
-        const response = await request(http)
-          [method.toLowerCase()]('/portfolios/42')
-          .set('Authorization', 'bearer ' + sessionToken)
-
-        expect(response.status).toBe(404)
-      },
-    )
-
-    test.each(getObjectsWithMissingAttribute(testPortfolio))(
-      'POST /portfolios fails if attribute %p is missing',
+  describe('POST /portfolios', () => {
+    it.each(getObjectsWithMissingAttribute(testPortfolio))(
+      'fails if attribute %p is missing',
       async (missingAttribute: string, portfolio) => {
         const response = await request(http)
           .post('/portfolios')
@@ -68,88 +72,111 @@ describe('Portfolios (e2e)', () => {
         )
       },
     )
-  })
 
-  describe('With one portfolio', () => {
-    let createResponse
-
-    beforeAll(async () => {
-      createResponse = await request(http)
+    it('returns portfolio with id', async () => {
+      const createResponse = await request(http)
         .post('/portfolios')
         .send(testPortfolio)
         .set('Authorization', 'bearer ' + sessionToken)
-    })
 
-    test('POST /portfolios returns portfolio with id', () => {
       expect(createResponse.status).toBe(201)
       expect(createResponse.body).toMatchObject(testPortfolio)
       expect(typeof createResponse.body.id).toBe('number')
     })
+  })
 
-    test('GET /portfolios returns single portfolio', async () => {
+  describe('GET/PUT/DELETE /portfolios', () => {
+    let portfolioId: number
+
+    beforeEach(async () => {
+      const createResponse = await request(http)
+        .post('/portfolios')
+        .send(testPortfolio)
+        .set('Authorization', 'bearer ' + sessionToken)
+
+      if (createResponse.status !== 201) {
+        throw new Error('Failed to create portfolio')
+      }
+
+      portfolioId = createResponse.body.id
+    })
+
+    test('GET /portfolios contains portfolio', async () => {
       const response = await request(http)
         .get('/portfolios')
         .set('Authorization', 'bearer ' + sessionToken)
 
       expect(response.status).toBe(200)
-      expect(response.body).toHaveLength(1)
-      expect(response.body).toMatchObject([testPortfolio])
+      expect(response.body).toContainEqual(
+        expect.objectContaining({
+          ...testPortfolio,
+          id: portfolioId,
+        }),
+      )
     })
 
     test('GET /portfolios/$id returns portfolio', async () => {
       const response = await request(http)
-        .get(`/portfolios/${createResponse.body.id}`)
+        .get(`/portfolios/${portfolioId}`)
         .set('Authorization', 'bearer ' + sessionToken)
 
       expect(response.status).toBe(200)
       expect(response.body).toMatchObject(testPortfolio)
     })
 
-    test('PUT /portfolios/$id updates portfolio', async () => {
-      const changedPortfolio = {
-        name: 'changed name',
-        note: 'different note',
-        baseCurrencyCode: 'USD',
-      }
+    describe('PUT /portfolios/$id', () => {
+      it.each(getObjectsWithMissingAttribute(testPortfolio))(
+        'fails if attribute %p is missing',
+        async (missingAttribute: string, portfolio) => {
+          const updateResponse = await request(http)
+            .put(`/portfolios/${portfolioId}`)
+            .send(portfolio)
+            .set('Authorization', 'bearer ' + sessionToken)
 
-      const updateResponse = await request(http)
-        .put(`/portfolios/${createResponse.body.id}`)
-        .send(changedPortfolio)
-        .set('Authorization', 'bearer ' + sessionToken)
+          expect(updateResponse.status).toBe(400)
+          expect(updateResponse.body.message).toContainEqual(
+            expect.stringContaining(missingAttribute),
+          )
+        },
+      )
 
-      expect(updateResponse.status).toBe(200)
-      expect(updateResponse.body).toMatchObject(changedPortfolio)
+      it('updates portfolio', async () => {
+        const changedPortfolio = {
+          name: 'changed name',
+          note: 'different note',
+          baseCurrencyCode: 'USD',
+        }
 
-      const getResponse = await request(http)
-        .get(`/portfolios/${createResponse.body.id}`)
-        .set('Authorization', 'bearer ' + sessionToken)
-
-      expect(getResponse.status).toBe(200)
-      expect(getResponse.body).toMatchObject(changedPortfolio)
-    })
-
-    test.each(getObjectsWithMissingAttribute(testPortfolio))(
-      'PUT /portfolios/$id fails if attribute %p is missing',
-      async (missingAttribute: string, portfolio) => {
         const updateResponse = await request(http)
-          .put(`/portfolios/${createResponse.body.id}`)
-          .send(portfolio)
+          .put(`/portfolios/${portfolioId}`)
+          .send(changedPortfolio)
           .set('Authorization', 'bearer ' + sessionToken)
 
-        expect(updateResponse.status).toBe(400)
-        expect(updateResponse.body.message).toContainEqual(
-          expect.stringContaining(missingAttribute),
-        )
-      },
-    )
+        expect(updateResponse.status).toBe(200)
+        expect(updateResponse.body).toMatchObject(changedPortfolio)
 
-    test('DELETE /portfolios/$id deletes portfolio', async () => {
+        const getResponse = await request(http)
+          .get(`/portfolios/${portfolioId}`)
+          .set('Authorization', 'bearer ' + sessionToken)
+
+        expect(getResponse.status).toBe(200)
+        expect(getResponse.body).toMatchObject(changedPortfolio)
+      })
+    })
+
+    test('DELETE /portfolios/$id removes portfolio', async () => {
       const response = await request(http)
-        .delete(`/portfolios/${createResponse.body.id}`)
+        .delete(`/portfolios/${portfolioId}`)
         .set('Authorization', 'bearer ' + sessionToken)
 
       expect(response.status).toBe(204)
       expect(response.body).toStrictEqual({})
+
+      const getResponse = await request(http)
+        .get(`/portfolios/${portfolioId}`)
+        .set('Authorization', 'bearer ' + sessionToken)
+
+      expect(getResponse.status).toBe(404)
     })
   })
 })
