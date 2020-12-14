bf80817fff62ae23df3b9862be25f4f8d1e58438
Thomas Fabritius <tfabritius@gmail.com>
2020-12-14 18:14:35 +0100

    refactor: remove unused import

--- src/currencies/exchangerate.query.ts
+++ src/currencies/exchangerate.query.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { ApiPropertyOptional } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
 import { IsOptional } from 'class-validator'
 import * as dayjs from 'dayjs'
