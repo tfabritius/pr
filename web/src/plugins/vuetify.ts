7f1913a17d61a05482866c8f7b465c57309fdecd
Thomas Fabritius <tfabritius@gmail.com>
2020-12-29 12:36:19 +0100

    feat(web): localize vuetify

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 
+import en from 'vuetify/src/locale/en'
+import de from 'vuetify/src/locale/de'
+
+import { getInitialLocale } from './i18n'
+
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  lang: {
+    current: getInitialLocale(),
+    locales: { de, en },
+  },
+})
