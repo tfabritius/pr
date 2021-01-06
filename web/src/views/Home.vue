20376d395358da8b1d83e7b546220672a666e2b8
Thomas Fabritius <tfabritius@gmail.com>
2021-01-06 16:35:28 +0100

    feat(web): show hint on home page if no portfolio is selected

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="home"></div>
+  <v-container class="d-flex justify-center">
+    <div style="width: 800px">
+      <div v-if="!$store.state.portfolio">
+        <v-alert color="info" outlined>
+          <v-icon color="info">
+            {{ mdiInformation }}
+          </v-icon>
+          {{ $t('home.no portfolio') }}
+        </v-alert>
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Mixins, Vue } from 'vue-property-decorator'
 
+import { IconsMixin } from '@/components/icons-mixin'
+import PortfoliosList from '@/components/PortfoliosList.vue'
+
+@Component({
+  components: {
+    PortfoliosList,
+  },
+})
 @Component
-export default class HomePage extends Vue {}
+export default class HomePage extends Mixins(Vue, IconsMixin) {}
 </script>
