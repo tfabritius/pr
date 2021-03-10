ff08b62931ce0e96152161f040de25dee80aa247
Thomas Fabritius <tfabritius@gmail.com>
2021-03-10 22:23:15 +0100

    refactor(api): remove unused imports

--- api/src/portfolios/transactions/transactions.module.ts
+++ api/src/portfolios/transactions/transactions.module.ts
@@ -3,16 +3,10 @@ import { forwardRef, Module } from '@nestjs/common'
 import { TransactionsController } from './transactions.controller'
 import { TransactionsService } from './transactions.service'
 import { PortfoliosModule } from '../portfolios.module'
-import { AccountsModule } from '../accounts/accounts.module'
-import { PortfolioSecuritiesModule } from '../securities/securities.module'
 import { PrismaService } from '../../prisma.service'
 
 @Module({
-  imports: [
-    forwardRef(() => PortfoliosModule),
-    forwardRef(() => AccountsModule),
-    forwardRef(() => PortfolioSecuritiesModule),
-  ],
+  imports: [forwardRef(() => PortfoliosModule)],
   providers: [TransactionsService, PrismaService],
   controllers: [TransactionsController],
 })
