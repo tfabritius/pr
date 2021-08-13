11e2d16f91174f7a8440bbe1d087ac9bcd7f0b42
Thomas Fabritius <tfabritius@gmail.com>
2021-08-13 17:06:44 +0200

    build: adapt eslint config

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,13 +1,24 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  extends: [],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   env: {
+    node: true,
     jest: true,
   },
   rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
     'plugin:vue/essential',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
+  env: {
+    node: true,
+  },
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
   },
   rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
     // Allow <template v-slot:foo.bar> or <template #foo.bar>
     'vue/valid-v-slot': [
       'error',
